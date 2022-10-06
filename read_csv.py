7f8d71501eb5ae84b2dfde50aebe74c05fe6b825
Pranav Senthilvel <pranavsenthil5@gmail.com>
2022-10-06 00:09:36 -0400

    added index.html

--- read_csv.py
+++ read_csv.py
@@ -1,12 +1,25 @@
 import csv
+from itertools import count
+import json
 from traceback import print_tb
+
+output_list = list()
+count = 1
 with open('covid_faq.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print("-"*100)
-        print("\n\n")
-        print("Question: " + row['questions'])
-        print("\n")
-        print("Answer: "+row['answers'])
-        print("\n\n")
-    print(reader)
+        row["id"] = str(count)
+        count += 1
+        output_list.append(row)
+        # print("-"*100)
+        # print("\n\n")
+        # print("Question: " + row['questions'])
+        # print("\n")
+        # print("Answer: "+row['answers'])
+        # print("\n\n")
+json_obj = json.dumps(output_list, indent=4)
+print(json_obj)
+
+jsonFile = open("covid_faq_data.json", "w")
+jsonFile.write(json_obj)
+jsonFile.close()
